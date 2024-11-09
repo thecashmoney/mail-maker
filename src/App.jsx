4f156d3d8732cb7a6c86f00dd4432a94bc074c4e
thecashmoney <elliottjang1@gmail.com>
2024-11-09 15:15:25 -0800

    changed tab sizes, started navbar

--- src/App.jsx
+++ src/App.jsx
@@ -5,73 +5,77 @@ import './App.css'
 import { googleLogout, useGoogleLogin } from '@react-oauth/google'
 import axios from 'axios';
 function App() {
-  const [ user, setUser ] = useState(null);
-  const [ profile, setProfile ] = useState(null);
-  const [rememberMe, setRememberMe] = useState(false);
+    const [ user, setUser ] = useState(null);
+    const [ profile, setProfile ] = useState(null);
+    const [rememberMe, setRememberMe] = useState(false);
 
-  //check if saved user is present- if so, open tht user.
-  useEffect(() => {
+    //check if saved user is present- if so, open tht user.
+    useEffect(() => {
     const savedUser = JSON.parse(localStorage.getItem('user'));
     if (savedUser) {
-      setUser(savedUser);
+        setUser(savedUser);
     }
-  }, []);
-
-  const login = useGoogleLogin({
+    }, []);
+    const login = useGoogleLogin({
     onSuccess: (codeResponse) => { setUser(codeResponse);
-      // store user in localstorage if remember me box is checked
-      if (rememberMe) {
+        // store user in localstorage if remember me box is checked
+        if (rememberMe) {
         localStorage.setItem('user', JSON.stringify(codeResponse));
-      }
+        }
     },
     onError: (error) => console.log('Login Failed:', error),
-  });
+    });
 
-  useEffect(
-      () => {
-          if (user) {
-              axios
-                  .get(`https://www.googleapis.com/oauth2/v1/userinfo?access_token=${user.access_token}`, {
-                      headers: {
-                          Authorization: `Bearer ${user.access_token}`,
-                          Accept: 'application/json'
-                      }
-                  })
-                  .then((res) => {setProfile(res.data);})
-                  .catch((err) => console.log(err));
-          }
-      },
-      [ user ]
-  );
+    useEffect(
+        () => {
+            if (user) {
+                axios.get(`https://www.googleapis.com/oauth2/v1/userinfo?access_token=${user.access_token}`, {
+                        headers: {
+                            Authorization: `Bearer ${user.access_token}`,
+                            Accept: 'application/json'
+                        }
+                    })
+                .then((res) => {setProfile(res.data);})
+                .catch((err) => console.log(err));
+            }
+        },
+        [ user ]
+    );
 
-  // log out function to log the user out of google and set the profile array to null
-  const logOut = () => {
-      googleLogout();
-      setProfile(null);
-      setUser(null);
-      localStorage.removeItem('user');
-  };
+    // log out function to log the user out of google and set the profile array to null
+    const logOut = () => {
+        googleLogout();
+        setProfile(null);
+        setUser(null);
+        localStorage.removeItem('user');
+    };
 
-  const handleRememberMeChange = (e) => {
+    const handleRememberMeChange = (e) => {
     setRememberMe(e.target.checked);
-  };
+    };
 
-  return (
-      <div>
-          {profile ? (
-              <div>
-                  <h2>hi {profile.name.split(' ')[0].toLowerCase()} !!!!</h2>
-                  <br />
-                  <br />
-                  <div class="profile"><img src={profile.picture} alt="user image" /></div>
-                  <h3>User Logged in</h3>
-                  <p>your email: {profile.email}</p>
-                  <br />
-                  <br />
-                  <button onClick={logOut}>Log out</button>
-              </div>
-          ) : (
-              <div>
+    return (
+        <div>
+            {profile ? (
+                <div>
+                    <nav>
+                    <div class="navitems">
+                        <div class="left">
+                        <a href="/" class="home-link">logo</a>
+                        </div>
+                        <div class="right">
+                        <p>{profile.email}</p>
+                        <img src={profile.picture} alt="user image" class="profile-pic" />
+                        </div>
+                    </div>
+                    </nav>
+                    <h2>hi {profile.name.split(' ')[0].toLowerCase()} !!!!</h2>
+                    <br />
+                    <br />
+                    <button onClick={logOut}>Log out</button>
+                </div>
+            ) : (
+                <div>
                 <h2>email maker !!!!</h2>
                 <br />
                 <h3>give a template, and we'll mass send emails!</h3>
@@ -81,19 +85,19 @@ function App() {
 
                 <br /><br /><br />
                 <div>
-                  <label>
+                    <label>
                     <input
-                      type="checkbox"
-                      checked={rememberMe}
-                      onChange={handleRememberMeChange}
+                        type="checkbox"
+                        checked={rememberMe}
+                        onChange={handleRememberMeChange}
                     />
                     Remember me !!!!
-                  </label>
+                    </label>
                 </div>
-              </div>
-          )}
-      </div>
-  );
+                </div>
+            )}
+        </div>
+    );
 }
 
 export default App;
