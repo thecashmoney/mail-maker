27086a2adf4c8285427fb2808cd2288c063dce28
thecashmoney <elliottjang1@gmail.com>
2024-11-09 13:25:33 -0800

    added basic google login !!

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,68 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-
+import { googleLogout, useGoogleLogin } from '@react-oauth/google'
+import axios from 'axios';
 function App() {
-  const [count, setCount] = useState(0)
+  const [ user, setUser ] = useState([]);
+  const [ profile, setProfile ] = useState([]);
+
+  const login = useGoogleLogin({
+      onSuccess: (codeResponse) => setUser(codeResponse),
+      onError: (error) => console.log('Login Failed:', error)
+  });
+
+  useEffect(
+      () => {
+          if (user) {
+              axios
+                  .get(`https://www.googleapis.com/oauth2/v1/userinfo?access_token=${user.access_token}`, {
+                      headers: {
+                          Authorization: `Bearer ${user.access_token}`,
+                          Accept: 'application/json'
+                      }
+                  })
+                  .then((res) => {
+                      setProfile(res.data);
+                  })
+                  .catch((err) => console.log(err));
+          }
+      },
+      [ user ]
+  );
+
+  // log out function to log the user out of google and set the profile array to null
+  const logOut = () => {
+      googleLogout();
+      setProfile(null);
+  };
 
   return (
-    <>
       <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+          {profile ? (
+              <div>
+                  <h2>hi {profile.name} !!!!</h2>
+                  <br />
+                  <br />
+                  <img src={profile.picture} alt="user image" />
+                  <h3>User Logged in</h3>
+                  <p>your email: {profile.email}</p>
+                  <br />
+                  <br />
+                  <button onClick={logOut}>Log out</button>
+              </div>
+          ) : (
+              <div>
+                <h2>email maker !!!!</h2>
+                <br />
+                <h3>give a template, and we'll mass send emails!</h3>
+                <br />
+                <button onClick={login}>sign in !!! (requires google account access) 🚀</button>
+              </div>
+          )}
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+  );
 }
 
-export default App
+export default App;
