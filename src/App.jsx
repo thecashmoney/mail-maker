025be95fa6c9ff5aaadd330e21984f3b87f343e9
Elliott Jang <elliottjang1@gmail.com>
2024-11-16 00:17:15 -0800

    added firebase !! revamped project :scream:

--- src/App.jsx
+++ src/App.jsx
@@ -1,120 +1,60 @@
+"use strict";
 import { useState, useEffect } from 'react'
 import './App.css'
-import { googleLogout, useGoogleLogin } from '@react-oauth/google'
 import Box from '@mui/material/Box';
 import TextField from '@mui/material/TextField';
-import axios from 'axios';
+import { useAuth, useSigninCheck, FirebaseAppProvider, FirestoreProvider, useFirestoreDocData, useFirestore, useFirebaseApp, AuthProvider } from 'reactfire';
+import { getAuth } from 'firebase/auth';
+import { GoogleAuthProvider, signInWithPopup } from "firebase/auth";
+
+
+const signOut = auth => auth.signOut().then(() => console.log('signed out'));
+const signIn = async auth => {
+    const provider = new GoogleAuthProvider();
+    await signInWithPopup(auth, provider);
+}
+
+
+function Auth() {
+    const { status, data: signinResult } = useSigninCheck();
+
+    if (status === 'loading') {
+        return <p>loading !!!</p>;
+    }
+
+    const { signedIn, user } = signinResult;
+    if (signedIn) {
+        console.log(user);
+        return (<SignedInHTML user={user} />);
+    } else return (<SignedOutHTML />);
+}
+
 function App() {
-    const [ user, setUser ] = useState(null);
-    const [ profile, setProfile ] = useState(null);
-    const [rememberMe, setRememberMe] = useState(false);
-    
+    const firebaseApp = useFirebaseApp();
+    const auth = getAuth(firebaseApp);
+    return (
+        <AuthProvider sdk={auth}>
+            <Auth />
+        </AuthProvider>
+    )
+}
+
+function SignedInHTML({ user }) {
+    const auth = useAuth();
     const [formValues, setFormValues] = useState({
         email: '',
         subject: '',
         body: ''
     });
-    
+
     const handleFormChange = (event) => {
         const { name, value } = event.target;
         setFormValues(prevValues => ({
-        ...prevValues,
-        [name]: value
+            ...prevValues,
+            [name]: value
         }));
     };
 
-    //gapi quickstart
-    // TODO(developer): Set to client ID and API key from the Developer Console
-    const CLIENT_ID = '348073828150-3tm7tsfgg51bm266t902nomfliad9qmo.apps.googleusercontent.com';
-    const API_KEY = user.access_token;
-
-    // Discovery doc URL for APIs used by the quickstart
-    const DISCOVERY_DOC = 'https://www.googleapis.com/discovery/v1/apis/gmail/v1/rest';
-
-    // Authorization scopes required by the API; multiple scopes can be
-    // included, separated by spaces.
-    const SCOPES = 'https://www.googleapis.com/auth/gmail.send ';
-
-    let tokenClient;
-    let gapiInited = false;
-    let gisInited = false;
-
-    document.getElementById('authorize_button').style.visibility = 'hidden';
-    document.getElementById('signout_button').style.visibility = 'hidden';
-
-    /**
-     * Callback after api.js is loaded.
-     */
-    function gapiLoaded() {
-        gapi.load('client', initializeGapiClient);
-    }
-
-    /**
-     * Callback after the API client is loaded. Loads the
-     * discovery doc to initialize the API.
-     */
-    async function initializeGapiClient() {
-        await gapi.client.init({
-            apiKey: API_KEY,
-            discoveryDocs: [DISCOVERY_DOC],
-        });
-        gapiInited = true;
-        maybeEnableButtons();
-    }
-
-    /**
-     * Callback after Google Identity Services are loaded.
-     */
-    function gisLoaded() {
-        tokenClient = google.accounts.oauth2.initTokenClient({
-        client_id: CLIENT_ID,
-        scope: SCOPES,
-        callback: '', // defined later
-    });
-        gisInited = true;
-        maybeEnableButtons();
-    }
-
-
-    //check if saved user is present- if so, open tht user.
-    useEffect(() => {
-    const savedUser = JSON.parse(localStorage.getItem('user'));
-    if (savedUser) {
-        setUser(savedUser);
-    }
-    }, []);
-    const login = useGoogleLogin({
-    onSuccess: (codeResponse) => { setUser(codeResponse);
-        // store user in localstorage if remember me box is checked
-        if (rememberMe) {
-        localStorage.setItem('user', JSON.stringify(codeResponse));
-        }
-    },
-    onError: (error) => console.log('Login Failed:', error),
-    });
-    useEffect(
-        () => {
-            if (user) {
-                axios.get(`https://www.googleapis.com/oauth2/v1/userinfo?access_token=${user.access_token}`, {
-                        headers: {
-                            Authorization: `Bearer ${user.access_token}`,
-                            Accept: 'application/json'
-                        }
-                    })
-                .then((res) => {setProfile(res.data);})
-                .catch((err) => console.log(err));
-            }
-        },
-        [ user ]
-    );
-
-    // log out function to log the user out of google and set the profile array to null
-    const logOut = () => {
-        googleLogout();
-        setProfile(null);
-        setUser(null);
-        localStorage.removeItem('user');
-    };
     const handleSubmit = (event) => {
         event.preventDefault();
     };
@@ -122,10 +62,10 @@ function App() {
     const sendEmail = () => {
         const { email, subject, body } = formValues;
         const message =
-        "From: " + profile.email + "\r\n" + 
-        "To: " + email + "\r\n" +
-        "Subject: " + subject + "\r\n\r\n" +
-        body;
+            "From: " + user.email + "\r\n" +
+            "To: " + email + "\r\n" +
+            "Subject: " + subject + "\r\n\r\n" +
+            body;
 
         console.log('Form Submitted:', formValues);
 
@@ -134,188 +74,137 @@ function App() {
 
         const reallyEncodedMessage = encodedMessage.replace(/\+/g, '-').replace(/\//g, '_').replace(/=+$/, '')
 
-        gapi.client.gmail.users.messages.send({
-            userId: 'me',
-            resource: { // Modified
-                // same response with any of these
-                raw: reallyEncodedMessage
-                // raw: encodedMessage
-                // raw: message
-            }
-        }).then(function () { console.log("done!");
-        }).catch((error) => {
-            console.error('Error sending email:', error);
-        });
+        //     gapi.client.gmail.users.messages.send({
+        //         userId: 'me',
+        //         resource: { // Modified
+        //             // same response with any of these
+        //             raw: reallyEncodedMessage
+        //             // raw: encodedMessage
+        //             // raw: message
+        //         }
+        //     }).then(function () {
+        //         console.log("done!");
+        //     }).catch((error) => {
+        //         console.error('Error sending email:', error);
+        //     });
     }
 
-    const handleRememberMeChange = (e) => {
-    setRememberMe(e.target.checked);
-    };
-    
+    return <div>
+        <nav>
+            <div class="navitems">
+                <div class="left">
+                    <a href="/" class="home-link">logo</a>
+                </div>
+                <div class="right">
+                    <p class="rightitem">hi {user.displayName.split(' ')[0].toLowerCase()} !!!! ({user.email})</p>
+                    <img src={user.photoURL} alt="user image" class="profile-pic" />
+                </div>
+            </div>
+        </nav>
+        <br /><br />
+        <h2>draft your email here !!</h2>
+        <br />
+        <form onSubmit={handleSubmit}>
+            <Box //for email, subject lines
+                component="form"
+                sx={{ '& > :not(style)': { m: 1, width: '25ch' } }}
+                noValidate
+                autoComplete="off"
+            >
+                <TextInput 
+                    name="email"
+                    label="email"
+                    id="outlined"
+                    value={formValues.email}
+                    onChange={handleFormChange}
+                />
+                <TextInput
+                    name = "subject"
+                    label = "subject"
+                    id = "outlined"
+                    value = {formValues.subject}
+                    onChange = {handleFormChange}
+                />
 
-    return (
-        <div>
-            {profile ? (
-                <div>
-                    <nav>
-                    <div class="navitems">
-                        <div class="left">
-                            <a href="/" class="home-link">logo</a>
-                        </div>
-                        <div class="right">
-                            <p class="rightitem">hi {profile.name.split(' ')[0].toLowerCase()} !!!! ({profile.email})</p>
-                            <img src={profile.picture} alt="user image" class="profile-pic" />
-                        </div>
-                    </div>
-                    </nav>
-                    <br /><br />
-                    <h2>draft your email here !!</h2>
-                    <br />
-                    <form onSubmit={handleSubmit}>
-                        <Box //for email, subject lines
-                        component="form"
-                        sx={{ '& > :not(style)': { m: 1, width: '25ch' } }}
-                        noValidate
-                        autoComplete="off"
-                        >
-                            <TextField
-                            name = "email"
-                            label="email"
-                            id="outlined"
-                            value={formValues.email}
-                            onChange={handleFormChange}
-                            sx={{
-                                '& label': {
-                                    color: '#A0AAB4',
-                                },
-                                '& label.Mui-focused': {
-                                    color: '#A0AAB4',
-                                },
-                                '& .MuiInput-underline:after': {
-                                    borderBottomColor: '#B2BAC2',
-                                },
-                                input: {
-                                    color: 'white',
-                                },
-                                '& .MuiOutlinedInput-root': {
-                                    '& fieldset': {
-                                        borderColor: '#E0E3E7',
-                                    },
-                                    '&:hover fieldset': {
-                                        borderColor: '#B2BAC2',
-                                    },
-                                    '&.Mui-focused fieldset': {
-                                        borderColor: '#6F7E8C',
-                                    },
-                                },
-                            }}
-                            />
-                            <TextField
-                            name = "subject"
-                            label="subject"
-                            id="outlined"
-                            value={formValues.subject}
-                            onChange={handleFormChange}
-                            sx={{
-                                '& label': {
-                                    color: '#A0AAB4',
-                                },
-                                '& label.Mui-focused': {
-                                    color: '#A0AAB4',
-                                },
-                                '& .MuiInput-underline:after': {
-                                    borderBottomColor: '#B2BAC2',
-                                },
-                                input: {
-                                    color: 'white',
-                                },
-                                '& .MuiOutlinedInput-root': {
-                                    '& fieldset': {
-                                        borderColor: '#E0E3E7',
-                                    },
-                                    '&:hover fieldset': {
-                                        borderColor: '#B2BAC2',
-                                    },
-                                    '&.Mui-focused fieldset': {
-                                        borderColor: '#6F7E8C',
-                                    },
-                                },
-                            }}
-                            />
-                        </Box>
-                        <Box  //for body
-                        component="form"
-                        sx={{ '& > :not(style)': { m: 1, width: '52ch' } }}
-                        noValidate
-                        autoComplete="off"
-                        >
-                            <TextField
-                            name = "body"
-                            id="outlined-multiline-static"
-                            label="body"
-                            multiline
-                            rows={4}
-                            //value={formValues.body}
-                            onChange={handleFormChange}
-                            sx={{
-                                '& label': {
-                                    color: '#A0AAB4',
-                                },
-                                '& label.Mui-focused': {
-                                    color: '#A0AAB4',
-                                },
-                                '& .MuiInput-underline:after': {
-                                    borderBottomColor: '#B2BAC2',
-                                },
-                                '& .MuiOutlinedInput-root': {
-                                    '& fieldset': {
-                                        borderColor: '#E0E3E7',
-                                    },
-                                    '&:hover fieldset': {
-                                        borderColor: '#B2BAC2',
-                                    },
-                                    '&.Mui-focused fieldset': {
-                                        borderColor: '#6F7E8C',
-                                    },
-                                    '& textarea': {
-                                        color: 'white', // Change text color inside the textarea
-                                    },
-                                },
-                            }}
-                            />
-                        </Box>
-                    </form>
-                    <button onClick={sendEmail}>send !!!</button>
-                    <br /><br />
-                    <button onClick={logOut}>Log out</button>
-                </div>
-            ) : (
-                <div>
-                <h2>mail maker !!!!</h2>
-                <br />
-                <h3>give a template, and we'll mass send emails!</h3>
-                <br />
-                <button onClick={login}>sign in !!! (requires google account access) 🚀</button>
+            </Box>
+            <Box  //for body
+                component="form"
+                sx={{ '& > :not(style)': { m: 1, width: '52ch' } }}
+                noValidate
+                autoComplete="off"
+            >
+                <TextInput
+                    name="body"
+                    id="outlined-multiline-static"
+                    label="body"
+                    multiline
+                    rows={4}
+                    onChange={handleFormChange}
+                />
+            </Box>
+        </form>
+        {/* <button onClick={sendEmail}>send !!!</button> */}
+        <br /><br />
+        <button onClick={() => signOut(auth)}>sign out !!</button>
+    </div>
+}
 
-                {/* remember me button */}
-                <br /><br /><br />
-                <div>
-                    <label>
-                    <input
-                        type="checkbox"
-                        checked={rememberMe}
-                        onChange={handleRememberMeChange}
-                    />
-                    Remember me !!!!
-                    </label>
-                </div>
-                <br /><br /><br />
-                <h3>NOTE</h3>
-                <p>Google will warn you that this is an unverified app. This is because our app will gain permission to send emails through your account. We will <b>only</b> use this power to send out group emails through your account and <b>nothing else.</b> Please continue with the app to be able to use it. It is hard for me to get verified because of the hard requirements (I need to make a privacy policy !!!! 😭)</p>
-                </div>
-            )}
-        </div>
-    );
+function SignedOutHTML() {
+    const auth = useAuth();
+    return <div>
+        <h2>mail maker !!!!</h2>
+        <br />
+        <h3>give a template, and we'll mass send emails!</h3>
+        <br />
+        <button onClick={() => signIn(auth)}>sign in !!! (requires google account access) 🚀</button>
+
+        <br /><br /><br />
+        <h3>NOTE</h3>
+        <p>Google will warn you that this is an unverified app. This is because our app will gain permission to send emails through your account. We will <b>only</b> use this power to send out group emails through your account and <b>nothing else.</b> Please continue with the app to be able to use it. It is hard for me to get verified because of the hard requirements (I need to make a privacy policy !!!! 😭)</p>
+    </div>
 }
 
 export default App;
+
+
+function TextInput({name, label, id, value, onChange, multiline=false, rows=4}) {
+    return (
+        <TextField
+            name={name}
+            label={label}
+            id={id}
+            value={value}
+            onChange={onChange}
+            multiline = {multiline}
+            rows = {rows}
+            sx={{
+                '& label': {
+                    color: '#A0AAB4',
+                },
+                '& label.Mui-focused': {
+                    color: '#A0AAB4',
+                },
+                '& .MuiInput-underline:after': {
+                    borderBottomColor: '#B2BAC2',
+                },
+                input: {
+                    color: 'white',
+                },
+                '& .MuiOutlinedInput-root': {
+                    '& fieldset': {
+                        borderColor: '#E0E3E7',
+                    },
+                    '&:hover fieldset': {
+                        borderColor: '#B2BAC2',
+                    },
+                    '&.Mui-focused fieldset': {
+                        borderColor: '#6F7E8C',
+                    },
+                    '& textarea': {
+                        color: 'white', // Change text color inside the textarea
+                    },
+                },
+            }}
+        />
+    );
+}
--- src/main.jsx
+++ src/main.jsx
@@ -1,18 +1,27 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import { GoogleOAuthProvider } from '@react-oauth/google'
+import { FirebaseAppProvider, FirestoreProvider, useFirestoreDocData, useFirestore, useFirebaseApp } from 'reactfire';
 import App from './App.jsx'
 import '@fontsource/roboto/300.css';
 import '@fontsource/roboto/400.css';
 import '@fontsource/roboto/500.css';
 import '@fontsource/roboto/700.css';
 
+const firebaseConfig = {
+  apiKey: "AIzaSyDruhafsII5ugYyEaitPWlfFQyUZpXr4ro",
+  authDomain: "mail-maker-1b4d9.firebaseapp.com",
+  projectId: "mail-maker-1b4d9",
+  storageBucket: "mail-maker-1b4d9.firebasestorage.app",
+  messagingSenderId: "440165704568",
+  appId: "1:440165704568:web:66e5b19b938b9b69c0de8d",
+  measurementId: "G-FSHGMFJHMF"
+};
 
 createRoot(document.getElementById('root')).render(
-  <GoogleOAuthProvider clientId = "348073828150-3tm7tsfgg51bm266t902nomfliad9qmo.apps.googleusercontent.com">
-    <StrictMode>
+  <StrictMode>
+    <FirebaseAppProvider firebaseConfig={firebaseConfig}>
       <App />
-    </StrictMode>
-  </GoogleOAuthProvider>,
+    </FirebaseAppProvider>
+  </StrictMode>
 )
